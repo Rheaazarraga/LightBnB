e47c14b3cb235ba54d40ded3053f2d99c074be0d
Rheaazarraga <rheaazarraga@gmail.com>
2021-10-16 05:58:47 +0000

    Add logic to clear existing views and append To for the newReservationForm

--- LightBnB_WebApp-master/public/javascript/views_manager.js
+++ LightBnB_WebApp-master/public/javascript/views_manager.js
@@ -5,12 +5,17 @@ $(() => {
   window.views_manager = {};
 
   window.views_manager.show = function(item, data = '') {
+    //clears out any existing views from the page with the detach() calls
     $newPropertyForm.detach();
     $propertyListings.detach();
     $searchPropertyForm.detach();
     $logInForm.detach();
     $signUpForm.detach();
+    // ensures that when switching from the reservation form view, that the information is cleared out of the main-content div.
+    $newReservationForm.detach();
 
+
+    // switch-case determines which view to display and the view is appended to the div with id of main-content.
     switch (item) {
       case 'listings':
         $propertyListings.appendTo($main);
@@ -27,6 +32,12 @@ $(() => {
       case 'signUp':
         $signUpForm.appendTo($main);
         break;
+      case 'newReservation':
+        const dataTag = `<h4>${data}</h4>`;
+        $newReservationForm.appendTo($main);
+        // a hidden tag is created with the id value of datatag, this is used when the reservation is being added to the database
+        $(dataTag).appendTo("#datatag");
+        break;
       case 'error': {
         const $error = $(`<p>${arguments[1]}</p>`);
         $error.appendTo('body');
@@ -34,6 +45,7 @@ $(() => {
           $error.remove();
           views_manager.show('listings');
         }, 2000);
+        
         
         break;
       }
